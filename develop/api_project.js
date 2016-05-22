163f44c2f7d0567717aae2fc91b2ad86fb612a0e
bnk-apidoc <bnk-apidoc@lschuermann.xyz>
2016-05-22 20:49:16 +0000

    initial commit

---
+++ develop/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "title": "bnk-core - API-Documentation",
+  "url": "https://your.bnk.instance",
+  "name": "bnk-core",
+  "version": "0.0.0",
+  "description": "An open-source debit-system for small communities.",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-22T20:43:48.115Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
