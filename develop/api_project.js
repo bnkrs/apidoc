5ce6399fce2ccb1128b564a179dc495ea42be893
bnk-apidoc <bnk-apidoc@lschuermann.xyz>
2016-07-24 10:26:07 +0000

    apidoc - generated at Sun Jul 24 10:26:07 UTC 2016

--- develop/api_project.js
+++ develop/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-07-24T10:13:46.320Z",
+    "time": "2016-07-24T10:26:06.564Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
